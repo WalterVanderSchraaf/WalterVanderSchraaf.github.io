91ce4d32d91b40b3e5771a7413a1f84934483629
WalterVanderSchraaf <WalterVanderSchraaf@yahoo.com>
2021-02-11 11:04:33 -0800

    removed _site from .gitignore, git add _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "My latest endeavor",
+        "excerpt":"This GitHub pages site is a work in progress while I learn the ins and outs of building a static site with Jekyll etc. Previously I’ve built sites based on Python (flask, django), RubyonRails, React/Nodejs. I’m a generalist and enjoy learning the different frameworks, stacks and new technology. I come...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2020/11/10/My-Latest-Endeavor.html",
+        "teaser": null
+      },{
+        "title": "my six words",
+        "excerpt":"Play Hard  Be Present  Forgive  Remember        If you had to summarize yourself in six words… Try it.   ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2020/11/11/My-Six-Words.html",
+        "teaser": null
+      }]
